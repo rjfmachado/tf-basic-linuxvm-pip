6e89073f28f8f39645c84f312666b8869fc8a2fc
rjfmachado <rjfmachado@users.noreply.github.com>
2020-11-09 15:53:10 +0000

    require tf 0.13

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
   required_providers {
     azurerm = "~> 2.0"
-  }
-}
-
-provider "azurerm" {
-  features {
-    virtual_machine {
-      delete_os_disk_on_deletion = true
-    }
   }
 }
